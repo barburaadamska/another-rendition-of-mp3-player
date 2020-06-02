11e12eec59d32f4f79710285c7b6d1c978199c92
barburaadamska <32602120+barburaadamska@users.noreply.github.com>
2020-06-02 11:28:31 +0200

    Add files via upload
    - init was shortened, additional methods added (namely pygame mixer initiation and UI elements creation)
    - added a main() function  which runs the program

--- wx_gui.py
+++ wx_gui.py
@@ -1,3 +1,17 @@
+# TODO:
+# - clock:
+#   - how many seconds since the track started playing
+#   - how many seconds left
+#   - self.clock = pygame.time.Clock()
+
+# TODO:
+# - integration with DirDialog (lines 157-169)
+
+# TODO:
+# - assign the value of working_directory to the folder above
+# - change the label on the UI
+# - *perhaps* add a playlist feature - an additional part of the UI with the .mp3 files listed
+
 # icons used are from icons8.com for the code to work you will need three bitmaps which will be assigned to icons,
 # as well as .mp3 files located in the same catalog as your .py code
 
@@ -7,47 +21,6 @@ import os
 import random
 
 
-# shortened button creation method
-def bitmap_button_creation(path, width, height, parent, position_x, position_y, toggle=False):
-    bitmap = wx.Bitmap(path)
-    image = bitmap.ConvertToImage()
-    image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
-    result = wx.Bitmap(image)
-    if not toggle:
-        button_created = wx.BitmapButton(parent, pos=(position_x, position_y), size=(height, width), bitmap=result)
-    else:
-        button_created = wx.BitmapToggleButton(parent, pos=(position_x, position_y), size=(height, width), label=result)
-    return button_created
-
-
-# shortened label creation
-def label_creation(parent, user_text, fontsize, position_x, position_y, color, weight=False):
-    label_created = wx.StaticText(parent, style=wx.ALIGN_CENTER, pos=(position_x, position_y))
-    label_text = str(user_text)
-    if not weight:
-        label_weight = wx.NORMAL
-    elif weight == "bold":
-        label_weight = wx.BOLD
-    elif weight == "light":
-        label_weight = wx.LIGHT
-    font = wx.Font(int(fontsize), wx.SWISS, wx.NORMAL, label_weight)
-    label_created.SetForegroundColour(color)
-    label_created.SetFont(font)
-    label_created.SetLabel(label_text)
-    return label_created
-
-# playlist creation method
-def playlist_creation(path, playlist = [], shuffle_var = False):
-    if not shuffle_var:
-        for filename in os.listdir(os.path.expanduser(path)):
-            if filename.endswith('.mp3'):
-                playlist.append(filename)
-        print(playlist)
-    else:
-        random.shuffle(playlist)
-        print(playlist)
-    return playlist
-
 # wx Frame
 class MyFrame(wx.Frame):
     def __init__(self):
@@ -55,26 +28,21 @@ class MyFrame(wx.Frame):
 
         super().__init__(parent=None, title='MP3 Player', size=(800, 300))
         panel = wx.Panel(self)
-        pygame.mixer.init()
-        self.SetBackgroundColour('#ffffff')
 
+        # PYGAME INITIATION (lines 39-52)
+        self.pygame_mixer_initiation()
 
+        # UI CREATION (lines 55-86)
+        self.UI_elements_creation(panel)
+
+    # PYGAME MIXER
+    def pygame_mixer_initiation(self):
         ### pygame shit goes here
-
-        # TODO:
-        # - clock:
-        #   - how many seconds since the track started playing
-        #   - how many seconds left
-        # self.clock = pygame.time.Clock()
-
         # where the music will be playing from
-        self.folder = '~/Desktop/moje_projektowe/'
-
-        # TODO:
-        # - integration with DirDialog (lines 157-169)
+        pygame.mixer.init()
+        self.folder = '~/Desktop/moje_projektowe/' # <- PATH TO THE PROJECT CATALOG
 
         # playlist creation for playback
-
         self.playlist = playlist_creation(self.folder)
 
         # index needed to navigate through the playlist
@@ -83,10 +51,12 @@ class MyFrame(wx.Frame):
         # initiation of pygame mixer
         pygame.mixer.music.load(self.playlist[self.i])
 
-        ### UI ELEMENTS GO HERE ###
+    # UI CREATION
+    def UI_elements_creation(self, panel):
+        # BG COLOR
+        self.SetBackgroundColour('#ffffff')
 
         # LEFT SIDE WITH LABELS
-
         self.playing_from_label = label_creation(panel, 'NOW PLAYING FROM', 13, 99, 54, '#707070')
         self.user_whereabouts_label = label_creation(panel, 'Twoja Stara in Desktop', 17, 99, 79, '#FF2D55')
 
@@ -94,19 +64,15 @@ class MyFrame(wx.Frame):
         self.artist_label = label_creation(panel, 'Artist Name', 17, 99, 157, '#707070')
 
         self.time_from_beginning_label = label_creation(panel, '06:15', 11, 99, 217, '#FF2D55')
-        self. song_length_label = label_creation(panel, '21:37', 11, 391, 217, '#FF2D55')
+        self.song_length_label = label_creation(panel, '21:37', 11, 391, 217, '#FF2D55')
 
-        # --------------------------------------
-
-        # RIGHT SIDE WITH BUTTONS & EVENT BINDS
-
+        # RIGHT SIDE WITH BUTTONS
         self.change_folder_button = wx.Button(panel, label='CHANGE FOLDER', pos=(510, 54), size=(183, 36))
         self.change_folder_button.Bind(wx.EVT_BUTTON, self.change_folder_button_clicked)
 
         self.backward_button = bitmap_button_creation('icon_backward.png', 40, 40, panel, 510, 130)
         self.backward_button.Bind(wx.EVT_BUTTON, self.backward_button_clicked)
 
-        # event binding for toggle button
         self.play_button = bitmap_button_creation('icon_play.png', 80, 80, panel, 560, 110, toggle=True)
         self.play_button.Bind(wx.EVT_TOGGLEBUTTON, self.toggle_button_clicked)
 
@@ -115,10 +81,11 @@ class MyFrame(wx.Frame):
 
         self.shuffle_button = bitmap_button_creation('icon-shuffle.png', 30, 30, panel, 585, 200, toggle=True)
         self.shuffle_button.Bind(wx.EVT_TOGGLEBUTTON, self.shuffle_button_clicked)
-        # --------------------------------------
 
+        # SHOWING UI ELEMENTS
         self.Show()
 
+    # BUTTON ACTIONS
     def toggle_button_clicked(self, event):
         # this returns a bool
         btn_var = event.GetEventObject().GetValue()
@@ -161,10 +128,6 @@ class MyFrame(wx.Frame):
         self.dialog = wx.DirDialog(None, "Choose a directory:", style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
         if self.dialog.ShowModal() == wx.ID_OK:
             self.working_directory = self.dialog.GetPath()
-            # TODO:
-            # - assign the value of working_directory to the folder above
-            # - change the label on the UI
-            # - *perhaps* add a playlist feature - an additional part of the UI with the .mp3 files listed
             print(self.dialog.GetPath())
         self.dialog.Destroy()
 
@@ -176,7 +139,54 @@ class MyFrame(wx.Frame):
             self.playlist = playlist_creation(self.folder, playlist=[])
 
 
-# initiation of the app
-app = wx.App()
-frame = MyFrame()
-app.MainLoop()
+# shortened button creation method
+def bitmap_button_creation(path, width, height, parent, position_x, position_y, toggle=False):
+    bitmap = wx.Bitmap(path)
+    image = bitmap.ConvertToImage()
+    image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+    result = wx.Bitmap(image)
+    if not toggle:
+        button_created = wx.BitmapButton(parent, pos=(position_x, position_y), size=(height, width), bitmap=result)
+    else:
+        button_created = wx.BitmapToggleButton(parent, pos=(position_x, position_y), size=(height, width), label=result)
+    return button_created
+
+
+# shortened label creation
+def label_creation(parent, user_text, fontsize, position_x, position_y, color, weight=False):
+    label_created = wx.StaticText(parent, style=wx.ALIGN_CENTER, pos=(position_x, position_y))
+    label_text = str(user_text)
+    if not weight:
+        label_weight = wx.NORMAL
+    elif weight == "bold":
+        label_weight = wx.BOLD
+    elif weight == "light":
+        label_weight = wx.LIGHT
+    font = wx.Font(int(fontsize), wx.SWISS, wx.NORMAL, label_weight)
+    label_created.SetForegroundColour(color)
+    label_created.SetFont(font)
+    label_created.SetLabel(label_text)
+    return label_created
+
+
+# playlist creation method
+def playlist_creation(path, playlist=[], shuffle_var=False):
+    if not shuffle_var:
+        for filename in os.listdir(os.path.expanduser(path)):
+            if filename.endswith('.mp3'):
+                playlist.append(filename)
+        print(playlist)
+    else:
+        random.shuffle(playlist)
+        print(playlist)
+    return playlist
+
+
+# a function which runs the program
+def main():
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
+
+
+main()
